9d38b8e81dd2648b1a17583ef44240cabfe2e7bd
danielvishna <73115554+danielvishna@users.noreply.github.com>
2020-12-26 10:47:02 +0200

    add SizeCompere.java, AbsCompere.java, TypeCompere.java

---
+++ src/filesprocessing/SizeCompere.java
@@ -0,0 +1,16 @@
+package filesprocessing;
+
+import java.io.File;
+
+public class SizeCompere implements ICompere{
+	@Override
+	public int compereFiles(File file1, File file2) {
+		if(file1.length() > file2.length()){
+			return 1;
+		}
+		if (file1.length() < file2.length()){
+			return -1;
+		}
+		return file1.getName().compareTo(file2.getName());
+	}
+}
---
+++ src/filesprocessing/TypeCompere.java
@@ -0,0 +1,24 @@
+package filesprocessing;
+import java.lang.*;
+import java.io.File;
+
+public class TypeCompere implements ICompere{
+	@Override
+	public int compereFiles(File file1, File file2) {
+		String extension1 = "";
+		String extension2 = "";
+
+		int fileOneLastIndexOfDot = file1.getName().lastIndexOf('.');
+		if (fileOneLastIndexOfDot > 0) {
+			extension1 = file1.getName().substring(fileOneLastIndexOfDot+1);
+		}
+		int fileTwoLastIndexOfDot = file2.getName().lastIndexOf('.');
+		if (fileTwoLastIndexOfDot > 0) {
+			extension2 = file1.getName().substring(fileTwoLastIndexOfDot+1);
+		}
+		if(extension1.compareTo(extension2) != 0){
+			return extension1.compareTo(extension2);
+		}
+		return file1.getName().compareTo(file2.getName());
+	}
+}
